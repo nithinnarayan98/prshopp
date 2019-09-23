218b36b0f81fab03c508bf909b4924c9c241db85
nithinnarayan98 <nithinnarayan98@gmail.com>
2019-09-23 20:15:12 +0530

    pr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule,Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const approutes:Routes=[{path:"",component:HomeComponent},{path:"login",component:LoginComponent}]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(approutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
